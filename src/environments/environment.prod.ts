fffae4441b0db789f9986b998b2ab6e03afe6e55
Mark Curran <S00199895@mail.itsligo.ie>
2021-03-26 10:29:42 +0000

    committed all files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAZ-wUi9W9S1dXJBH53cr1khzbef6hA2Z0",
+    authDomain: "carsales-3ab8a.firebaseapp.com",
+    projectId: "carsales-3ab8a",
+    storageBucket: "carsales-3ab8a.appspot.com",
+    messagingSenderId: "681998990559",
+    appId: "1:681998990559:web:5717a955584663064abd6a"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAZ-wUi9W9S1dXJBH53cr1khzbef6hA2Z0",
+    authDomain: "carsales-3ab8a.firebaseapp.com",
+    projectId: "carsales-3ab8a",
+    storageBucket: "carsales-3ab8a.appspot.com",
+    messagingSenderId: "681998990559",
+    appId: "1:681998990559:web:5717a955584663064abd6a"
+  }
 };
 
 /*
